66e065fdff5473c5b3876591374964a59d475a8b
pollysgithub <89196488867pb@gmail.com>
2022-06-27 22:57:35 +0300

    add files

---
+++ demo/Admin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    /// <summary>
+    /// Логика взаимодействия для Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+            using (SkiResortEntities db= new SkiResortEntities())
+            {
+                StaffGrid.ItemsSource = db.Staff.ToList();
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow MW = new MainWindow();
+            MW.Show();
+            this.Close();
+        }
+    }
+}
---
+++ demo/QrCode.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using ZXing;
+using ZXing.Common;
+
+namespace demo
+{
+    /// <summary>
+    /// Логика взаимодействия для QrCode.xaml
+    /// </summary>
+    public partial class QrCode : Window
+    {
+        Window _window;
+        public QrCode(Orders order, Window window)
+        {
+            InitializeComponent();
+            imageBar.Source = null;
+            _window = window;
+            OrderCode.Text = order.Order_code;
+            GeneratorBar(order.Order_code);
+        }
+        private System.Drawing.Image GeneratorBar(string msg)
+        {
+            MultiFormatWriter mutiWriter = new MultiFormatWriter();
+            BitMatrix bm = mutiWriter.encode(msg, BarcodeFormat.CODE_39, 350, 256);
+            Bitmap img = new BarcodeWriter().Write(bm);
+            imageBar.Source = BitmapToBitmapImage(img);
+            return img;
+        }
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Png);
+                stream.Position = 0;
+                BitmapImage result = new BitmapImage();
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();
+                return result;
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            
+            Seller seller = new Seller(_window);
+            seller.Show();
+            this.Close();
+        }
+    }
+}
